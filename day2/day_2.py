6844e2d024b746908605afd3b76e11f4c43d300b
Philip Siederer <philipsi@axis.com>
2019-12-04 12:58:47 +0100

    day2 part 2 ugly
    Change-Id: I9296ccc55bcb745db5cb3c32a8cb96c7e5599555

---
+++ day2/day_2.py
@@ -0,0 +1,22 @@
+import operator
+
+op = {
+   1: operator.add,
+   2: operator.mul,}
+
+with open("day2/input_day2.txt") as f:
+    data = [int(i) for i in f.read().split(",")]
+    data_copy = data.copy()
+    for i in range(0,100):
+        for j in range(0,100):
+            data = data_copy.copy()
+            data[1] = i
+            data[2] = j
+            ptr = 0
+            while data[ptr] != 99:
+                # print(data)
+                data[data[ptr+3]] = op[data[ptr]](data[data[ptr+1]],data[data[ptr+2]])
+                ptr += 4
+            if data[0] == 19690720:
+                print("Answer is: {}".format(100 * i + j))
+                exit(1)
