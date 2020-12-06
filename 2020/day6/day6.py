44372d4fc5b6187296a729a667a42a6dbf0c4557
Philip Siederer <philipsi@axis.com>
2020-12-06 11:13:47 +0100

    day6 complete
    Change-Id: Ica6cfc2ed4a0da27370f45356a1a8ccca29aab09

---
+++ 2020/day6/day6.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import re
+
+def get_uniques(inp):
+    return set(re.findall(r'(\w)',inp))
+
+def get_question_all_answered(group):
+    return set.intersection(*(get_uniques(person) for person in group.split('\n')))
+
+def run_test():
+    assert get_uniques('abc') == {'a','b','c'}
+    assert get_uniques('a\nb\nc') == {'a','b','c'}
+    assert get_uniques('ab\nbc') == {'a','b','c'}
+    assert get_uniques('a\na\na') == {'a'}
+
+    assert get_question_all_answered('abc') == {'a','b','c'}
+    assert get_question_all_answered('a\nb\nc') == set()
+    assert get_question_all_answered('ab\nbc') == {'b'}
+    assert get_question_all_answered('a\na\na') == {'a'}
+run_test()
+
+with open(Path("2020") / "day6" / "day6_input.txt") as f:
+    groups = [line for line in f.read().split("\n\n")]
+
+ans1 = sum([len(get_uniques(g)) for g in groups])
+print(f'Part 1: {ans1}')
+
+ans2 = sum([len(get_question_all_answered(g)) for g in groups])
+print(f'Part 2: {ans2}')
