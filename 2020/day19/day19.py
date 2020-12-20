ea0e461f87563800cfeb0fbb7fd4d8ed7cd0a5c7
Philip <philip@example.com>
2020-12-20 10:21:32 +0100

    day19 part1 complete

---
+++ 2020/day19/day19.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+from pathlib import Path
+import re
+def parse_input(inp):
+    print(inp)
+    rules_raw, msges = inp.split('\n\n')
+    print(rules_raw)
+    rules = defaultdict(list)
+    for rule in rules_raw.splitlines():
+        print(rule)
+        rule_nr, ru = rule.split(': ')
+        for rule_part in ru.split('|'):
+            rules[rule_nr].append(rule_part)
+    print('+++++')
+    messages = [line for line in msges.splitlines()]
+    return rules,messages
+
+def generate_regex(rules, key,rules_map={}):
+    if key in rules_map:
+        return rules_map[key]
+    elif rules[key] == ['\"a\"']:
+        return 'a'
+    elif rules[key] == ['\"b\"']:
+        return 'b'
+    reg = "("
+    for rule in rules[key]:
+        for num in re.findall(r'\d+', rule):
+            reg += generate_regex(rules,num,rules_map)
+        reg += '|'
+    final = reg[:-1]+ ')'
+    rules_map[key] = final
+    return final
+
+
+
+
+def run_test():
+    inp = """0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb"""
+    rules , messages= parse_input(inp)
+    reg = generate_regex(rules,'0')
+    print([msg for msg in messages if re.fullmatch(r'{}'.format(reg),msg)])
+
+# run_test()
+
+with open(Path("2020") / "day19" / "day19_input.txt") as f:
+    inp = f.read()
+rules , messages= parse_input(inp)
+reg = generate_regex(rules,'0')
+print(reg)
+print(len([msg for msg in messages if re.fullmatch(r'{}'.format(reg),msg)]))
