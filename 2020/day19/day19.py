bfddef52c643bb30595ba1689d453d3ba289a157
Philip <philip@example.com>
2020-12-20 11:13:27 +0100

    cleanup

--- 2020/day19/day19.py
+++ 2020/day19/day19.py
@@ -1,27 +1,23 @@
 from collections import defaultdict
 from pathlib import Path
 import re
+
 def parse_input(inp,part2=False):
-    # print(inp)
     rules_raw, msges = inp.split('\n\n')
-    # print(rules_raw)
     rules = defaultdict(list)
     for rule in rules_raw.splitlines():
-        print(rule)
         rule_nr, ru = rule.split(': ')
         for rule_part in ru.split('|'):
             rules[rule_nr].append(rule_part)
-    # print('+++++')
     if part2:
         rules['8'] = ['42 ',' 42 8']
         rules['11']= ['42 31 ','42 11 31 ']
-        # rule[]
-    # print(rules)
     messages = [line for line in msges.splitlines()]
     return rules,messages
 
-rules_map={}
-def generate_regex(rules, key,max_depth=1000, depth = 0):
+def generate_regex(rules,key,rules_map=None,max_depth=1000, depth = 0):
+    if rules_map == None:
+        rules_map = {}
     if key in rules_map:
         return rules_map[key]
     elif rules[key] == ['\"a\"']:
@@ -34,39 +30,23 @@ def generate_regex(rules, key,max_depth=1000, depth = 0):
     reg = "("
     for rule in rules[key]:
         for num in re.findall(r'\d+', rule):
-            reg += generate_regex(rules,num,max_depth,depth+1)
+            reg += generate_regex(rules,num,rules_map,max_depth,depth+1)
         reg += '|'
     final = reg[:-1]+ ')'
     rules_map[key] = final
     return final
 
-
-
-
-def run_test():
-    inp = """0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
-
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb"""
-    rules , messages= parse_input(inp)
-    reg = generate_regex(rules,'0')
-    print([msg for msg in messages if re.fullmatch(r'{}'.format(reg),msg)])
-
-# run_test()
+def is_message_valid(reg,msg):
+    return re.fullmatch(r'{}'.format(reg),msg) != None
 
 with open(Path("2020") / "day19" / "day19_input.txt") as f:
     inp = f.read()
-rules , messages= parse_input(inp,part2=True)
+rules , messages= parse_input(inp)
 max_len = max([len(msg) for msg in messages])
-print(max_len)
+# print(max_len)
 reg = generate_regex(rules,'0',max_depth=max_len)
-# print(reg)
-print(len([msg for msg in messages if re.fullmatch(r'{}'.format(reg),msg)]))
+print(f"Number of valid messages part1: {sum([is_message_valid(reg,msg) for msg in messages])}")
+
+rules , messages= parse_input(inp,part2=True)
+reg = generate_regex(rules,'0',max_depth=max_len)
+print(f"Number of valid messages part2: {sum([is_message_valid(reg,msg) for msg in messages])}")
