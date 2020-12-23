75c76c938eb58bb7a0224603f0d6602cbde61d0b
Philip Siederer <philipsi@axis.com>
2020-12-23 15:20:42 +0100

    day23 complete

--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -1,49 +1,77 @@
 from collections import deque
 
-def step(cups):
+# def step(cups):
+#     # print(cups)
+#     pickup = cups[1:4]
+#     # print('Pickup', pickup)
+#     dest = cups[0] -1
+#     new = [cups[0]] + cups[4:]
+#     # print(new)
+#     # print(dest)
+#     while dest not in new:
+#         dest -=1
+#         if dest <= 0:
+#             dest = max(cups)
+#     # print('destination', dest)
+#     ind = new.index(dest)
+#     # print('index', ind)
+#     new[ind+1:ind+1] = pickup
+#     return new[1:] + new[:1]
+
+def play(cups, steps=10,part2=False):
+    # This is a mess. circle is a dict abstracting a circular single linked list.
+    # Each key holds the clockwise neighbor
+    MAX_CUP = max(cups)
+    circle = {}
     # print(cups)
-    pickup = cups[1:4]
-    # print('Pickup', pickup)
-    dest = cups[0] -1
-    new = [cups[0]] + cups[4:]
-    # print(new)
-    # print(dest)
-    while dest not in new:
-        dest -=1
-        if dest <= 0:
-            dest = max(cups)
-    # print('destination', dest)
-    ind = new.index(dest)
-    # print('index', ind)
-    new[ind+1:ind+1] = pickup
-    return new[1:] + new[:1]
+    for i in range(len(cups)-1):
+        circle[cups[i]] = cups[i+1]
+    circle[cups[-1]] = cups[0]
+    current_cup = cups[0]
 
-def score(cups):
-    ind = cups.index(1)
-    s = cups[ind+1:] + cups[:ind]
-    return int("".join(map(str,s)))
+    for _ in range(steps):
+        picked_up = []
+        next_cup = circle[current_cup]
+        for _ in range(3):
+            picked_up.append(next_cup)
+            next_cup = circle[next_cup]
+        circle[current_cup] = next_cup
+        dest = current_cup -1
+        while dest in picked_up or dest == 0:
+            dest -=1
+            if dest <= 0:
+                dest = MAX_CUP
+        circle[picked_up[-1]] = circle[dest]
+        circle[dest] = picked_up[0]
+        current_cup = next_cup
+    if part2:
+        return score2(circle)
+    else:
+        return score(circle)
 
-def score2(cups):
-    ind = cups.index(1)
-    print(cups[ind+1],cups[ind+2])
-    return cups[ind+1] * cups[ind+2]
+def score(circle):
+    val = circle[1]
+    s = ""
+    while val != 1:
+        s = s + str(val)
+        val = circle[val]
+    return int(s)
+
+def score2(circle):
+    val = circle[1]
+    val2 = circle[val]
+    return val * val2
+
 def run_test():
     cups = list(map(int,'389125467'))
-    for _ in range(10):
-        cups = step(cups)
-    # print(cups)
-    assert score(cups) == 92658374
+    assert play(cups,10) == 92658374
 
-    cups = list(map(int,'389125467'))
-    cups.extend([i for i in range(max(cups),1000001)])
-    print(max(cups))
-    for _ in range(10000000):
-        cups = step(cups)
-    print(score2(cups))
+    cups.extend([i for i in range(10,1000001)])
+    assert play(cups,10000000,part2=True) == 149245887792
 
 run_test()
 
-# cups = list(map(int,'739862541'))
-# for _ in range(100):
-#     cups = step(cups)
-# print(score(cups))
+cups = list(map(int,'739862541'))
+print(f'Answer part 1: {play(cups,100)}')
+cups.extend([i for i in range(10,1000001)])
+print(f'Answer part 2: {play(cups,10000000,part2=True)}')
