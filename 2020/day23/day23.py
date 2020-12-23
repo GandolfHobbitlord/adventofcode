cdc010c0a35d22e7defbfa3566e996812a684b90
Philip Siederer <philipsi@axis.com>
2020-12-23 16:56:08 +0100

    more cleanup

--- 2020/day23/day23.py
+++ 2020/day23/day23.py
@@ -1,22 +1,3 @@
-from collections import deque
-
-# def step(cups):
-#     # print(cups)
-#     pickup = cups[1:4]
-#     # print('Pickup', pickup)
-#     dest = cups[0] -1
-#     new = [cups[0]] + cups[4:]
-#     # print(new)
-#     # print(dest)
-#     while dest not in new:
-#         dest -=1
-#         if dest <= 0:
-#             dest = max(cups)
-#     # print('destination', dest)
-#     ind = new.index(dest)
-#     # print('index', ind)
-#     new[ind+1:ind+1] = pickup
-#     return new[1:] + new[:1]
 
 def play(cups, steps=10,part2=False):
     # This is a mess. circle is a dict abstracting a circular single linked list.
