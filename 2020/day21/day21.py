fb637b845886f4796d74d8305c4897645b346fca
Philip Siederer <philipsi@axis.com>
2020-12-25 14:21:10 +0100

    day 21 lazy cleanup

--- 2020/day21/day21.py
+++ 2020/day21/day21.py
@@ -3,42 +3,38 @@ from collections import Counter
 from pathlib import Path
 def part1(inp):
     cont = {}
-    c = Counter()
+    ingr_counter = Counter()
     all_ingr = set()
     for line in inp.splitlines():
-        ingr_str, conaminents = line.split(' (contains ')
+        ingr_str, contaminets = line.split(' (contains ')
         ingr = set([ingredient for ingredient in ingr_str.split(' ')])
         all_ingr |= ingr
-        c.update(ingr)
-        allergens = [c for c in re.findall(r'\w+',conaminents)]
+        ingr_counter.update(ingr)
+        allergens = [allergen for allergen in re.findall(r'\w+',contaminets)]
         for allergen in allergens:
             if allergen in cont:
                 cont[allergen] = set.intersection(cont[allergen], ingr)
             else:
                 cont[allergen] = ingr
     no_allergen_ingr = all_ingr - set().union(*cont.values())
-    return sum([c[a] for a in no_allergen_ingr])
+    return sum([ingr_counter[ingredient] for ingredient in no_allergen_ingr])
 
 def part2(inp):
     cont = {}
-    c = Counter()
     all_ingr = set()
     for line in inp.splitlines():
-        ingr_str, conaminents = line.split(' (contains ')
+        ingr_str, contaminets = line.split(' (contains ')
         ingr = set([ingredient for ingredient in ingr_str.split(' ')])
         all_ingr.update(ingr)
-        c.update(ingr)
-        allergens = [c for c in re.findall(r'\w+',conaminents)]
+        allergens = [c for c in re.findall(r'\w+',contaminets)]
         for allergen in allergens:
             if allergen in cont:
                 cont[allergen] = set.intersection(cont[allergen], ingr)
             else:
                 cont[allergen] = ingr
-    # print(all_ingr)
-    # print(cont)
-    # print(c)
     answers = []
     taken = set()
+    # Real part2 here. The stuff before is copy paste from part1, to lazy to clean today
     while len(taken) != len(cont.keys()):
         for allergen, ingr in cont.items():
             remaining = ingr - taken
@@ -46,13 +42,9 @@ def part2(inp):
                 elem = remaining.pop()
                 answers.append((allergen,elem))
                 taken.add(elem)
-    print(answers)
     answers.sort(key=lambda x:x[0])
-    print('HEELOOOO')
-    print(','.join([ingr for _, ingr in answers]))
     return ','.join([ingr for _, ingr in answers])
-    # print(answers[:3])
-    # return sum([c[a] for a in no_allergen_ingr])
+
 def run_test():
     inp = """mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
@@ -64,5 +56,5 @@ sqjhc mxmxvkd sbzzf (contains fish)"""
 run_test()
 with open(Path("2020") / "day21" / "day21_input.txt") as f:
     inp = f.read()
-    print(part1(inp))
-    print(part2(inp))
+print(f'Answer part 1: {part1(inp)}')
+print(f'Answer part 2: {part2(inp)}')
