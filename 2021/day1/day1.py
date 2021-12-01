59e8382bfe63517e66bf6c979f63e0fa81547a18
Philip Siederer <philipsi@axis.com>
2021-12-01 10:54:06 +0100

    Use clever trick for day 1 part 2
    Change-Id: I58fa64c5bcae3c0b83f3e0cab486890b438d6c01

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,19 +1,13 @@
 from pathlib import Path
 
-def part_1(nums):
-   print(sum([old < new for old, new in zip(nums, nums[1:])]))
+def part_1(depths):
+   return sum([old < new for old, new in zip(depths, depths[1:])])
 
-def part_2(nums):
-   increase = 0
-   for i in range(3,len(num)):
-      old = sum(num[i-3:i])
-      new = sum(num[i-2:i+1])
-      if old < new:
-         increase += 1
-   print(increase)
+def part_2(depths):
+   return sum([old < new for old, new in zip(depths, depths[3:])])
 
 with open(Path("2021") / "day1" / "day1_input.txt") as f:
-   num = [int(x) for x in f.read().splitlines()]
+   depths = [int(depth) for depth in f.read().splitlines()]
 
-part_1(num)
-part_2(num)
+print(f"Answer of part 1 is {part_1(depths)}")
+print(f"Answer of part 2 is {part_2(depths)}")
