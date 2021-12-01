8ac5636adcc4817b6d1c455d7795b9d1b3c6cafd
Philip <philip@example.com>
2021-12-01 08:30:13 +0100

    slightly cleaner part1

--- 2021/day1/day1.py
+++ 2021/day1/day1.py
@@ -1,20 +1,19 @@
 from pathlib import Path
+
+def part_1(nums):
+   print(sum([old < new for old, new in zip(nums, nums[1:])]))
+
+def part_2(nums):
+   increase = 0
+   for i in range(3,len(num)):
+      old = sum(num[i-3:i])
+      new = sum(num[i-2:i+1])
+      if old < new:
+         increase += 1
+   print(increase)
+
 with open(Path("2021") / "day1" / "day1_input.txt") as f:
    num = [int(x) for x in f.read().splitlines()]
 
-increase = 0
-decrease = 0
-# for i in range(1,len(num)):
-#    if num[i-1] < num[i]:
-#       increase += 1
-#    else:
-#       decrease += 1
-
-for i in range(3,len(num)):
-   prev = sum(num[i-3:i])
-   now = sum(num[i-2:i+1])
-   if prev < now:
-      increase += 1
-   else:
-      decrease += 1
-print(increase)
+part_1(num)
+part_2(num)
