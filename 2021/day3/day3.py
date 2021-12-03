adb5aaf60ec8141ab9effd13cc75b4195daff5f8
Philip <philip@example.com>
2021-12-04 00:40:30 +0100

    complete day3 after some beers

--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -1,9 +1,7 @@
 from pathlib import Path
-import re
 
 def to_bin(bin):
     x = [pow(2,i) if x == 1 else 0 for i, x in enumerate(reversed(bin))]
-    print(x)
     return sum(x)
 
 def part1(data):
@@ -11,7 +9,7 @@ def part1(data):
     ans = [1 if x.count('1') > x.count('0') else 0 for x in types]
     ans0 = to_bin(ans)
     ans1 = to_bin([0 if x else 1 for x in ans])
-    print(ans0*ans1)
+    return ans0*ans1
 
 def most_common(lis, index):
     x = [line[index] for line in lis]
@@ -26,32 +24,27 @@ def least_common(lis, index):
     else:
         return '1'
 
-def get_co2(data):
+def get_support_rating(data, criteria):
     keep = data.copy()
     for i in range(len(data[0])):
-        common = least_common(keep, i)
+        common = criteria(keep, i)
         keep = [line for line in keep if line[i] == common]
         if len(keep) == 1:
-            print(f"done with co2 {keep}")
             return to_bin([int(i) for i in keep[0]])
 
 def get_oxygen(data):
-    keep = data.copy()
-    for i in range(len(data[0])):
-        common = most_common(keep, i)
-        keep = [line for line in keep if line[i] == common]
-        if len(keep) == 1:
-            print(f"done with oxygen {keep}")
-            return to_bin([int(i) for i in keep[0]])
+   return get_support_rating(data, most_common)
+
+def get_co2(data):
+    return get_support_rating(data, least_common)
 
 def part2(data):
-    keep = data.copy()
     ox = get_oxygen(data)
     co2 = get_co2(data)
-    print(co2 * ox)
-
+    return co2 * ox
 
 with open(Path("2021") / "day3" / "day3_input.txt") as f:
    data = [line for line in f.read().splitlines()]
-part1(data)
-part2(data)
+
+print(f'Answer part1 {part1(data)}')
+print(f'Answer part2 {part2(data)}')
