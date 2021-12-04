8cd0e2d0ac85198bcad1b1f4072a9b3914519424
Philip <philip@example.com>
2021-12-04 10:54:22 +0100

    cleanup

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -2,24 +2,24 @@ from pathlib import Path
 import numpy as np
 
 MARK = -1 # No negative numbers so use that as marked
-def mark(num, boards):
+def mark(boards, num):
     for board in boards:
         board[board == num] = MARK
 
-def run_tests():
-    with open(Path("2021") / "day4" / "day4_input.txt") as f:
-        inp = [line for line in f.read().split('\n\n')]
-    # print(numbers)
+def parse_data(inp):
     numbers = [int(num) for num in inp[0].split(',')]
     boards = []
     for board in inp[1:]:
         boards.append(np.fromstring(board,sep=' ', dtype='int8').reshape(-1, 5))
         print(boards[-1])
         print('\n')
+    return boards, numbers
 
+def part1(data):
+    boards, numbers = parse_data(data)
     for num in numbers:
         print(num)
-        mark(num, boards)
+        mark(boards, num)
         for board in boards:
             if winner(board):
                 print("Winner")
@@ -38,4 +38,7 @@ def winner(board):
     for column in board.T:
         if np.all(column ==  MARK):
             return True
-run_tests()
+
+with open(Path("2021") / "day4" / "day4_input.txt") as f:
+    inp = [line for line in f.read().split('\n\n')]
+part1(inp)
