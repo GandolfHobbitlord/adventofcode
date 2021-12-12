a335615a69efa59e0f5e0ed50b0368d4d1b9713d
Philip <philip@example.com>
2021-12-12 23:16:01 +0100

    really ugly day 12, clean up tomorrow

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -8,7 +8,8 @@ def create_connection_from_input(input):
         connections[dest].append(src)
     return connections
 
-def get_numb_ways(conn, src = 'start', visited = None, path = None):
+#path just for debug
+def get_numb_ways(conn, src = 'start', visited = None, part1 = True, path = None):
     if not path:
         path = []
     if not visited:
@@ -18,14 +19,20 @@ def get_numb_ways(conn, src = 'start', visited = None, path = None):
     if src.islower():
         visited.append(src)
     for dest in conn[src]:
-        if dest == 'end':
+        if dest == 'start':
+            continue
+        elif dest == 'end':
             path.append(dest)
-            print(path)
             total +=1
-        elif dest in conn and not dest in visited:
-                total += get_numb_ways(conn, dest, visited.copy(), path.copy())
+        else:
+            if dest in visited:
+                if part1:
+                    continue
+                else:
+                    total += get_numb_ways(conn, dest, visited.copy(), True, path.copy())
+            else:
+                total += get_numb_ways(conn, dest, visited.copy(), part1, path.copy())
     return total
-
 
 def part1(input):
     conn = create_connection_from_input(input)
@@ -33,11 +40,13 @@ def part1(input):
 
 def part2(input):
     conn = create_connection_from_input(input)
-    return get_numb_ways(conn)
+    return get_numb_ways(conn, part1=False)
+
 def run_tests():
     input = 'start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end'
     conn = create_connection_from_input(input)
-    assert 10 == get_numb_ways(conn)
+    #assert 10 == get_numb_ways(conn)
+    assert 36 == part2(input)
     input = 'dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc'
     conn = create_connection_from_input(input)
     assert 19 == get_numb_ways(conn)
@@ -45,3 +54,4 @@ def run_tests():
 run_tests()
 input = 'start-kc\npd-NV\nstart-zw\nUI-pd\nHK-end\nUI-kc\npd-ih\nih-end\nstart-UI\nkc-zw\nend-ks\nMF-mq\nHK-zw\nLF-ks\nHK-kc\nih-HK\nkc-pd\nks-pd\nMF-pd\nUI-zw\nih-NV\nks-HK\nMF-kc\nzw-NV\nNV-ks'
 print(part1(input))
+print(part2(input))
