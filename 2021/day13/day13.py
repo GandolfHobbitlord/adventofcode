f04d34ec1cfde5ea7e37941cd8174387345a3ee8
Philip <philip@example.com>
2021-12-13 23:09:19 +0100

    day 13 complete

--- 2021/day13/day13.py
+++ 2021/day13/day13.py
@@ -1,38 +1,37 @@
 import numpy as np
 import re
 from pathlib import Path
-def to_matrix(mat_raw):
-    points = re.findall('(\d+),(\d+)', mat_raw)
-    points = [(int(x), int(y)) for x,y in points]
-    max_x = max([x for x,_ in points])
-    max_y = max([y for _,y in points])
-    # FIX ME Should look at size of folds to determine matrix. max_x + 1 worked for test but not for input
-    mat = np.zeros((895,max_x+1), dtype=int)
+
+def to_matrix(mat_raw, folds):
+    points = [(int(x), int(y)) for x,y in re.findall('(\d+),(\d+)', mat_raw)]
+    max_x = max([line for axis , line in folds if axis == 'x'])
+    max_y = max([line for axis , line in folds if axis == 'y'])
+    output = np.zeros((max_y * 2 + 1,max_x * 2 + 1), dtype=int)
     for x, y in points:
-        mat[y,x] = 1
-    return mat
+        output[y,x] = 1
+    return output
 
 def parse_data(input):
     raw_points, raw_fold = input.split('\n\n')
-    folds = re.findall('fold along (\w)=(\d+)', raw_fold)
-    mat = to_matrix(raw_points)
-    return mat, folds
+    folds = [(axis, int(line)) for axis, line in re.findall('fold along (\w)=(\d+)', raw_fold)]
+    paper = to_matrix(raw_points, folds)
+    return paper, folds
 
 def fold_paper(paper, fold):
-    print(paper.shape)
     axis, line = fold
-    line = int(line)
     if axis == 'y':
-        flippy = np.flipud(paper[line+1:,:])
-        folded = paper[: line, :] | flippy
+        folded = paper[:line, :] | np.flipud(paper[line+1:, :])
         return folded
     if axis == 'x':
-        flippy = np.fliplr(paper[:,line+1:])
-        folded = paper[:,:line] | flippy
+        folded = paper[:, :line] | np.fliplr(paper[:, line+1:])
         return folded
+
+def print_paper(paper):
+    return '\n'.join([' '.join(['#' if val == 1 else " " for val in row]) for row in paper])
 
 def count_dots(paper):
     return np.sum(paper)
+
 def run_tests():
     with open(Path("2021") / "day13" / "day13_test.txt") as f:
         paper, folds = parse_data(f.read())
@@ -42,13 +41,14 @@ def run_tests():
         ans = np.array([[1, 1, 1, 1, 1],[1, 0, 0, 0, 1],[1, 0, 0, 0, 1],[1, 0, 0, 0, 1],[1, 1, 1, 1, 1],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0]])
         assert np.all(paper == ans)
 
-# run_tests()
+run_tests()
 with open(Path("2021") / "day13" / "day13_input.txt") as f:
     paper, folds = parse_data(f.read())
-# print(f"answer part 1 {count_dots(fold_paper(paper,folds[0]))}")
+
+print(f"answer part 1 {count_dots(fold_paper(paper,folds[0]))}")
+
 for fold in folds:
     paper = fold_paper(paper,fold)
     P= paper ==1
 
-print('\n'.join([' '.join(['#' if val == 1 else "." for val in row]) for row in
-    paper]))
+print(f"Answer part 2: \n{print_paper(paper)}")
