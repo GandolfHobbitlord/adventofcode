9d524fa043b17a2eedf5858c0cf918038a55a6eb
Philip Siederer <philipsi@axis.com>
2021-12-14 17:55:32 +0100

    day14 closer
    Change-Id: I977e9a963aaec9bf19390dada33858a7921927e2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -12,24 +12,31 @@ def parse_data(input):
     return counter, rules
 
 def step(string_template, rules, steps=1):
-    next = Counter()
     chars = Counter(string_template)
     template = string2counter(string_template)
-    for key, val in template.items():
-        if key in rules:
-            added_letter = rules[key]
-            next[key[0] + added_letter] += val
-            next[added_letter + key[1]] += val
-            chars.update(added_letter)
-        else:
-            next[key] += val
-    return next
-
+    for _ in range(steps):
+        next = Counter()
+        for key, val in template.items():
+            if key in rules:
+                added_letter = rules[key]
+                next[key[0] + added_letter] += val
+                next[added_letter + key[1]] += val
+                chars.update(added_letter)
+            else:
+                next[key] += val
+        template = next.copy()
+    return next, chars
+def score(chars):
+    return max(chars.values()) - min(chars.values())
 def run_tests():
     with open(Path("2021") / "day14" / "day14_test.txt") as f:
         template, rules = parse_data(f.read())
-    step1 = step(template, rules)
+    step1, chars1 = step(template, rules)
     assert step1 == string2counter('NCNBCHB')
-    # step2 = step(template, rules)
-    # assert step == string2counter('NBCCNBBBCBHCB')
+    assert chars1 == Counter('NCNBCHB')
+    step2, chars2 = step(template, rules, steps=2)
+    assert step2 == string2counter('NBCCNBBBCBHCB')
+    assert chars2 == Counter('NBCCNBBBCBHCB')
+    _, chars = step(template, rules, steps=10)
+    print(score(chars))
 run_tests()
