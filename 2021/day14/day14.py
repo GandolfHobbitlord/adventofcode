1610c6b180bbb26246f2cd46d65c4c6f2388c895
Philip <philip@example.com>
2021-12-14 19:11:19 +0100

    day14 complete

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 import re
 from collections import Counter
+
 def string2counter(string):
     counter = Counter()
     for match in re.findall(r'(?=(\w\w))', string):
         counter.update([match])
     return counter
+
 def parse_data(input):
     counter = input.splitlines()[0]
     rules = {key:add for key, add in re.findall('(\w+) -> (\w)', input)}
     return counter, rules
 
-def step(string_template, rules, steps=1):
+def do_steps(string_template, rules, steps):
     chars = Counter(string_template)
     template = string2counter(string_template)
     for _ in range(steps):
@@ -32,25 +34,24 @@ def score(chars):
 def run_tests():
     with open(Path("2021") / "day14" / "day14_test.txt") as f:
         template, rules = parse_data(f.read())
-    step1, chars1 = step(template, rules)
+    step1, chars1 = do_steps(template, rules, steps=1)
     assert step1 == string2counter('NCNBCHB')
     assert chars1 == Counter('NCNBCHB')
-    step2, chars2 = step(template, rules, steps=2)
+    step2, chars2 = do_steps(template, rules, steps=2)
     assert step2 == string2counter('NBCCNBBBCBHCB')
     assert chars2 == Counter('NBCCNBBBCBHCB')
-    _, chars = step(template, rules, steps=10)
+    _, chars = do_steps(template, rules, steps=10)
     assert score(chars) == 1588
 
 run_tests()
 
 def part1(template, rules):
-    _, chars = step(template, rules, steps=10)
+    _, chars = do_steps(template, rules, steps=10)
     return score(chars)
 
 def part2(template, rules):
-    _, chars = step(template, rules, steps=40)
+    _, chars = do_steps(template, rules, steps=40)
     return score(chars)
-
 
 with open(Path("2021") / "day14" / "day14_input.txt") as f:
     template, rules = parse_data(f.read())
