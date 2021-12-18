03df6d98579037c8ebfc9fb4f8a7b3e0d06a66dc
Philip Siederer <philipsi@axis.com>
2021-12-18 22:23:11 +0100

    cleanup

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -1,5 +1,9 @@
 import numpy as np
 from pathlib import Path
+
+import time
+start_time = time.time()
+
 def to_matrix(inp):
     lines = [line for line in inp.splitlines()]
     mat=[]
@@ -21,28 +25,27 @@ def get_neighbor(mat, pos):
         if in_range(mat,n):
             yield n
 
-def create_graph(mat):
-    distances = np.full(mat.shape, np.inf)
+def find_shortest_path(risk_levels):
+    tot_risk = np.full(risk_levels.shape, np.inf)
     q = []
     q.append((0,0))
-    distances[(0,0)] = 0
+    tot_risk[(0,0)] = 0
     visited = set()
-    while q:
+    finish_pos = (risk_levels.shape[0]-1 ,risk_levels.shape[1] -1)
+    while finish_pos not in visited:
         vertice = q.pop()
         if vertice in visited:
             continue
-        curr_dist = distances[vertice]
-        for neigh_pos in get_neighbor(mat,vertice):
-            new_distance = curr_dist + mat[neigh_pos]
-            if new_distance < distances[neigh_pos]:
-                distances[neigh_pos] = new_distance
-        for neighbor in get_neighbor(mat,vertice):
+        curr_dist = tot_risk[vertice]
+        for neighbor in get_neighbor(risk_levels,vertice):
+            new_distance = curr_dist + risk_levels[neighbor]
+            if new_distance < tot_risk[neighbor]:
+                tot_risk[neighbor] = new_distance
             if (neighbor not in visited):
                 q.append(neighbor)
         visited.add(vertice)
-        q = sorted(q, key=lambda x: distances[x], reverse=True)
-    # print(distances)
-    return distances[-1,-1]
+        q = sorted(q, key=lambda x: tot_risk[x], reverse=True)
+    return tot_risk[finish_pos]
 
 def tile(mat):
     mat = np.concatenate([mat + i for i in range(5)],axis=0)
@@ -62,8 +65,8 @@ def run_tests():
 1293138521
 2311944581"""
     mat = to_matrix(input)
-    assert create_graph(mat) == 40
-    assert create_graph(tile(mat.copy())) == 315
+    assert find_shortest_path(mat) == 40
+    assert find_shortest_path(tile(mat.copy())) == 315
     a = np.ones((1,1)) *8
     print(a)
     print(tile(a))
@@ -71,5 +74,6 @@ run_tests()
 
 with open(Path("2021") / "day15" / "day15_input.txt") as f:
     dangers = to_matrix(f.read())
-    print(create_graph(dangers))
-    print(create_graph(tile(dangers.copy())))
+    print(find_shortest_path(dangers))
+    print(find_shortest_path(tile(dangers.copy())))
+print("--- %s seconds ---" % (time.time() - start_time))
