b0f23053c58ba7a76427f1514187d5b97bf4773c
Philip Siederer <philipsi@axis.com>
2021-12-21 23:07:04 +0100

    day16 done

--- 2021/day16/day16.py
+++ 2021/day16/day16.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import math
 bin_parse ={ '0' : '0000',
 '1' : '0001',
 '2' : '0010',
@@ -30,16 +31,14 @@ def run(data, pos=0):
     if typeID == 4:
         val = ''
         while True:
-            # print(data[1:5])
             is_end = data[pos] == '0'
             pos += 1
             val += data[pos:pos+4]
             pos += 4
             if is_end:
-                print(f'literal value {int(val,2)}')
-                return pos
-        # print(val)
+                return pos, int(val,2)
     else:
+        values = []
         len_id = data[pos]
         pos += 1
         if len_id == '0':
@@ -48,42 +47,63 @@ def run(data, pos=0):
             pos += bit_len
             end_point = pos + tot_len
             while end_point > pos:
-                pos = run(data, pos)
-            return pos
+                pos, val = run(data, pos)
+                values.append(val)
         else:
             bit_len = 11
             num_of_packets = int(data[pos:pos+bit_len],2)
             pos += bit_len
-            print(f'num of subpackets {num_of_packets}')
             for _ in range(num_of_packets):
-                print('sub_packet')
-                pos = run(data,pos)
-            return pos
+                pos, val = run(data,pos)
+                values.append(val)
 
-def parse_line(line):
+        if typeID == 0:
+            return pos, sum(values)
+        elif typeID == 1:
+            return pos, math.prod(values)
+        elif typeID == 2:
+            return pos, min(values)
+        elif typeID == 3:
+            return pos, max(values)
+        elif typeID == 5:
+            assert len(values) == 2
+            return pos, int(values[0] > values[1])
+        elif typeID == 6:
+            assert len(values) == 2
+            return pos, int(values[0] < values[1])
+        elif typeID == 7:
+            assert len(values) == 2
+            return pos, int(values[0] == values[1])
+
+
+def from_hex(line):
     return "".join([bin_parse[char] for char in line])
 
 def run_tests():
     inp = 'D2FE28'
-    bin = parse_line(inp)
-    # assert bin == '110100101111111000101000'
-    # run(bin)
-    # run(parse_line('38006F45291200'))
-    # run(parse_line('EE00D40C823060'))
-    # run(parse_line('8A004A801A8002F478'))
-    # assert version_sum == 16
+    bin = from_hex(inp)
+    assert bin == '110100101111111000101000'
+    run(bin)
+    run(from_hex('38006F45291200'))
+    run(from_hex('EE00D40C823060'))
+    run(from_hex('8A004A801A8002F478'))
+    assert version_sum == 16
 
-    run(parse_line('620080001611562C8802118E34'))
+    run(from_hex('620080001611562C8802118E34'))
     assert version_sum == 12
 
-    run(parse_line('C0015000016115A2E0802F182340'))
+    run(from_hex('C0015000016115A2E0802F182340'))
     assert version_sum == 23
 
-    run(parse_line('A0016C880162017C3686B18A3D4780'))
+    run(from_hex('A0016C880162017C3686B18A3D4780'))
     assert version_sum == 31
+
+    _, ret = run(from_hex('C200B40A82'))
+    assert ret == 3
 run_tests()
 
 with open(Path("2021") / "day16" / "day16_input.txt") as f:
-    bin = parse_line(f.read())
-run(bin)
+    bin = from_hex(f.read())
+_, ret = run(bin)
+print(ret)
 print(version_sum)
