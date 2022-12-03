b1c1bc797e763c03d3e738ce57630b7e3a27ac74
Philip <philip@example.com>
2022-12-03 10:38:21 +0100

    cleanup day 3

--- 2022/day3/day3.py
+++ 2022/day3/day3.py
@@ -10,24 +10,16 @@ def part_1(data):
     for line in data:
         p1 = line[len(line)//2:]
         p2 = line[:len(line)//2]
-        c1 = Counter(p1)
-        c2 = Counter(p2)
-        intersec = c1 & c2
+        intersec = Counter(p1) & Counter(p2)
         common_char = intersec.most_common(1)[0][0]
-        # print(common_char)
         ans += score[common_char]
     return ans
 
 def part_2(data):
     ans = 0
     for p1, p2, p3 in zip(data[::3], data[1::3], data[2::3]):
-        c1 = Counter(p1)
-        c2 = Counter(p2)
-        c3 = Counter(p3)
-
-        intersec = c1 & c2 & c3
+        intersec = Counter(p1) & Counter(p2) & Counter(p3)
         common_char = intersec.most_common(1)[0][0]
-        # print(common_char)
         ans += score[common_char]
     return ans
 
