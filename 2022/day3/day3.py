baba9f0b3e195bf398495541fae7cbfe60f6ae16
Philip <philip@example.com>
2022-12-03 06:49:05 +0100

    day3 complete

--- 2022/day3/day3.py
+++ 2022/day3/day3.py
@@ -1,29 +1,38 @@
-import re
 from pathlib import Path
 from collections import Counter
-def parse_line(line):
-    m = re.match(r'(\w+) (\d+)', line)
-    dir, distance = m.groups()
-    return dir, int(distance)
+
 score = {}
 for i, l in enumerate('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',1):
     score[l] = i
-print(score)
+
+def part_1(data):
+    ans = 0
+    for line in data:
+        p1 = line[len(line)//2:]
+        p2 = line[:len(line)//2]
+        c1 = Counter(p1)
+        c2 = Counter(p2)
+        intersec = c1 & c2
+        common_char = intersec.most_common(1)[0][0]
+        # print(common_char)
+        ans += score[common_char]
+    return ans
+
+def part_2(data):
+    ans = 0
+    for p1, p2, p3 in zip(data[::3], data[1::3], data[2::3]):
+        c1 = Counter(p1)
+        c2 = Counter(p2)
+        c3 = Counter(p3)
+
+        intersec = c1 & c2 & c3
+        common_char = intersec.most_common(1)[0][0]
+        # print(common_char)
+        ans += score[common_char]
+    return ans
+
 with open(Path("2022") / "day3" / "day3_input.txt") as f:
-    #data = [parse_line(x) for x in f.read().splitlines()]
     data = [x for x in f.read().splitlines()]
-    l = list()
-    for string in data:
-        p1 = string[:len(string)//2]
-        p2 = string[len(string)//2:]
-        l.append((p1,p2))
 
-ans = 0
-for p1, p2 in l:
-    c1 = Counter(p1)
-    c2 = Counter(p2)
-    c3 = c1 & c2
-    f = c3.most_common(1)
-    ans += score[f[0][0]]
-
-print(ans)
+print(f"Answer part 1: {part_1(data)}")
+print(f"Answer part 2: {part_2(data)}")
