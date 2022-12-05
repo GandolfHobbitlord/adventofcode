9975d2dd4a019d69f69dd00fb11c908f914b9c5d
Philip Siederer <philipsi@axis.com>
2022-12-05 08:37:40 +0100

    clean up day5
    Change-Id: Id69548b7f87cba64438d4364067d9fc7f3ce58ba

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -1,54 +1,49 @@
 import re
 from pathlib import Path
-
-
-def part1(moves, stacks):
-    for num_pop, start, dest in moves:
-        print(f"{num_pop}=")
-        for i in range(num_pop):
-            stacks[dest].append(stacks[start].pop())
-    return score(stacks)
-
-def part2(moves, stacks):
-    for num_pop, start, dest in moves:
-        print(f"{num_pop}=")
-        new_stack = []
-        for i in range(num_pop):
-            new_stack.insert(0,stacks[start].pop())
-
-        stacks[dest].extend(new_stack)
-    return score(stacks)
+import copy
 
 def parse_crates(crates):
-    crates = crates.splitlines()
+    num_bins = max(get_numbers(crates))
     stacks = list()
-    for i in range(20):
+    for _ in range(num_bins+1):
         stacks.append([])
 
+    crates = crates.splitlines()
     for line in crates[:-1]:
         for num, char in enumerate(line[1::4],1):
             if char != ' ':
                 stacks[num].insert(0,char)
     return stacks
 
-def parse_moves(line):
-    m = re.findall(r'\d+', line)
-    return [int(x) for x in m]
+def get_numbers(line):
+    return [int(val) for val in re.findall(r'\d+', line)]
 
 def score(stacks):
-    score =""
-    for s in stacks:
-        if len(s) !=0:
-            score += s[-1]
-    print(score)
+    score =  "".join([s[-1] for s in stacks if len(s) != 0 ])
+    return score
+
+def part1(moves, stacks):
+    stacks = copy.deepcopy(stacks)
+    for num_pop, start, dest in moves:
+        for i in range(num_pop):
+            stacks[dest].append(stacks[start].pop())
+    return score(stacks)
+
+def part2(moves, stacks):
+    stacks = copy.deepcopy(stacks)
+    for num_pop, start, dest in moves:
+        new_stack = []
+        for i in range(num_pop):
+            new_stack.insert(0,stacks[start].pop())
+        stacks[dest].extend(new_stack)
+    return score(stacks)
 
 with open(Path("2022") / "day5" / "day5_input.txt") as f:
     crates, moves = [x for x in f.read().split('\n\n')]
-stacks = parse_crates(crates)
-print(moves)
-moves = [parse_moves(move) for move in moves.splitlines()]
-print(stacks)
 
-part1(moves.copy(),stacks.copy())
-part2(moves.copy(),stacks.copy())
+stacks = parse_crates(crates)
+moves = [get_numbers(move) for move in moves.splitlines()]
+
+print(f"Answer Part 1: {part1(moves,stacks)}")
+print(f"Answer Part 2: {part2(moves,stacks)}")
 # print(stacks)
