3600a25c862eb86e98289d8167443e17f14afdbf
Philip <philip@example.com>
2022-12-10 07:20:33 +0100

    day10

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -8,21 +8,29 @@ def print_screen(arr):
     for row in arr:
         print(''.join(row))
 
-X = [1]
-for line in data:
-    cmd = line.split()
-    X.append(X[-1])
-    if cmd[0] == 'addx':
-        X.append(X[-1] + int(cmd[1]))
+def get_signals(data):
+    X = [1]
+    for line in data:
+        cmd = line.split()
+        X.append(X[-1])
+        if cmd[0] == 'addx':
+            X.append(X[-1] + int(cmd[1]))
+    return X
 
-get_signals = [20, 60, 100, 140, 180, 220]
-print(f'Answer part 1 {sum([X[x-1]*x for x in get_signals])}')
+def part1(data):
+    X = get_signals(data)
+    sig = [20, 60, 100, 140, 180, 220]
+    print(f'Answer part 1 {sum([X[x-1]*x for x in sig])}')
 
-#Part 2
-screen = np.full((6, 40), ' ')
-for cycle, sprit_pos in enumerate(X):
-    x = cycle % 40
-    y = cycle // 40
-    if sprit_pos -1  <= x and x <= sprit_pos + 1:
-        screen[y, x] = '#'
-print_screen(screen)
+def part2(data):
+    X = get_signals(data)
+    screen = np.full((6, 40), ' ')
+    for cycle, sprite_pos in enumerate(X):
+        x = cycle % 40
+        y = cycle // 40
+        if sprite_pos -1  <= x and x <= sprite_pos + 1:
+            screen[y, x] = '#'
+    print_screen(screen)
+
+part1(data)
+part2(data)
