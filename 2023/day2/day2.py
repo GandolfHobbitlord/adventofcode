8e5bc9b3739e93d7bc5e4773dda8621797153ef6
Philip Siederer <philipsi@axis.com>
2023-12-02 15:56:47 +0100

    part2 done

--- 2023/day2/day2.py
+++ 2023/day2/day2.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import re
+import numpy as np
 from collections import defaultdict
 def to_dict(line):
     _, games = line.split(':')
@@ -16,9 +17,13 @@ def to_dict(line):
 
 with open(Path("2023") / "day2" / "day2_input.txt") as f:
     data = [to_dict(x) for x in f.read().splitlines()]
-    ok = []
+    ans1 = 0
     for i, game in enumerate(data,1):
         if game['red'] <= 12 and game['green'] <= 13 and game['blue'] <= 14:
-            ok.append(i)
+            ans1 += i
+    ans2 = 0
+    for i, game in enumerate(data,1):
+        ans2 += np.prod(list(game.values()))
 
-    print(sum(ok))
+    print(ans1)
+    print(ans2)
