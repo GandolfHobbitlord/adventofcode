4f0ca5433391f4f131eb0a55cd4df8044e682da8
Philip Siederer <philipsi@axis.com>
2023-12-06 18:18:55 +0100

    day 5 complete

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -3,6 +3,7 @@ import re
 from collections import defaultdict
 from collections import Counter
 import numpy as np
+from itertools import chain
 
 
 def parse_map(map):
@@ -21,11 +22,12 @@ def get_conversion(curr,map):
 def get_seeds_p1(seed_line):
     return [int(x) for x in re.findall(r'\d+',seed_line)]
 
-# def get_seeds_p2(seed_line):
-#     ss = list(([int(x) for x in re.findall(r'\d+',seed_line)]))
-
-#     print(ss)
-#     exit()
+def get_seeds_p2(seed_line):
+    r = []
+    ss = list(([int(x) for x in re.findall(r'\d+',seed_line)]))
+    for start, ran in zip(ss[::2], ss[1::2]):
+        r.append(range(start,start+ran))
+    return chain.from_iterable(r)
 #     return
 with open(Path("2023") / "day5" / "day5_input.txt") as f:
 # with open(Path("2023") / "day5" / "day5_test.txt") as f:
@@ -34,17 +36,21 @@ with open(Path("2023") / "day5" / "day5_input.txt") as f:
     data = [line for line in f.read().split('\n\n')]
 
 # seeds = get_seeds_p1(data[0])
-seeds = get_seeds_p1(data[0])
+seeds = get_seeds_p2(data[0])
 print(seeds)
 maps  = [parse_map(m) for m in data[1:]]
 print(maps)
 locations = []
+min_location = 100000000000000000000
 for s in seeds:
     curr = s
     for map in maps:
         n = get_conversion(curr,map)
-        print(f'{curr}->{n}')
+        # print(f'{curr}->{n}')
         curr = n
-    locations.append(curr)
+    # locations.append(curr)
+    min_location = min(min_location,curr)
+
+print(min_location)
 print(locations)
 print(min(locations))
