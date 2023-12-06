1f02f73af4ddf575f1b85a967be3cfcb571487fe
Philip Siederer <philipsi@axis.com>
2023-12-06 18:30:36 +0100

    some cleanup day5

--- 2023/day5/day5.py
+++ 2023/day5/day5.py
@@ -14,11 +14,13 @@ def parse_map(map):
         dest, origin, ran = [int(x) for x in re.match(r'(\d+) (\d+) (\d+)', line).groups()]
         range_list.append((dest,origin,ran))
     return range_list
+
 def get_conversion(curr,map):
     for dest, origin, ran in map:
         if curr >= origin and curr < origin + ran:
             return dest + curr - origin
     return curr
+
 def get_seeds_p1(seed_line):
     return [int(x) for x in re.findall(r'\d+',seed_line)]
 
@@ -31,26 +33,26 @@ def get_seeds_p2(seed_line):
 #     return
 with open(Path("2023") / "day5" / "day5_input.txt") as f:
 # with open(Path("2023") / "day5" / "day5_test.txt") as f:
-
-    # data = [parse_line(line) for line in f.read().splitlines()]
     data = [line for line in f.read().split('\n\n')]
 
-# seeds = get_seeds_p1(data[0])
-seeds = get_seeds_p2(data[0])
-print(seeds)
-maps  = [parse_map(m) for m in data[1:]]
-print(maps)
-locations = []
-min_location = 100000000000000000000
-for s in seeds:
-    curr = s
-    for map in maps:
-        n = get_conversion(curr,map)
-        # print(f'{curr}->{n}')
-        curr = n
-    # locations.append(curr)
-    min_location = min(min_location,curr)
 
-print(min_location)
-print(locations)
-print(min(locations))
+maps  = [parse_map(m) for m in data[1:]]
+def get_min_location(seeds):
+    min_location = 100000000000000000000
+    for s in seeds:
+        curr = s
+        for map in maps:
+            n = get_conversion(curr,map)
+            # print(f'{curr}->{n}')
+            curr = n
+        min_location = min(min_location,curr)
+    return min_location
+
+seeds = get_seeds_p1(data[0])
+print(f'Part 1: {get_min_location(seeds)}')
+seeds = get_seeds_p2(data[0])
+print(f'Part 2: {get_min_location(seeds)}')
+
+# print(min_location)
+# print(locations)
+# print(min(locations))
