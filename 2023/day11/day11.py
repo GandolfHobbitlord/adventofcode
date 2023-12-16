6388bef2f9fb84bb120ccc8449672d8688af6986
Philip Siederer <philipsi@axis.com>
2023-12-16 10:10:43 +0100

    day11 complete

--- 2023/day11/day11.py
+++ 2023/day11/day11.py
@@ -1,32 +1,39 @@
 from pathlib import Path
 import numpy as np
-import re
 from itertools import combinations
-def parse_line(line):
-    m = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
-    lo, hi, char, password = m.groups()
-    return int(lo), int(hi), char, password
 
 with open(Path("2023") / "day11" / "day11_input.txt") as f:
 # with open(Path("2023") / "day11" / "day11_test.txt") as f:
     data = np.array([list(line) for line in f.read().split('\n')])
 
-def expand_rows(data):
-    tmp = []
-    for row in data:
-        tmp.append(row.copy())
-        if np.all(row == '.'):
-            tmp.append(row.copy())
-    return np.vstack(tmp)
+def expand_galaxy(data,expansion = 1):
+    positions = np.array(np.where(data == '#'))
+    yy,xx = np.where(data == '#')
+    xx = np.array(xx)
+    yy = np.array(yy)
 
-data = expand_rows(data)
-data = expand_rows(data.T).T
+    empty_rows = [row_nr for row_nr, row in enumerate(data) if np.all(row == '.')]
+    adders = np.zeros_like(yy)
+    for y in empty_rows:
+        adders[y<yy] += expansion
+    yy += adders
 
-rows, cols = np.where(data == '#')
-positions = [(x,y) for y,x in zip(rows,cols)]
+    empty_cols = [row_nr for row_nr, row in enumerate(data.T) if np.all(row == '.')]
+    adders = np.zeros_like(xx)
+    for x in empty_cols:
+        adders[x<xx] += expansion
+    xx += adders
+    positions = [(x,y) for x,y in zip(xx,yy)]
+    return positions
 
-ans = 0
-for (x0,y0), (x1,y1) in combinations(positions,2):
-    distance = abs(x0-x1) + abs(y0-y1)
-    ans += distance
-print(ans)
+def total_distance(positions):
+    tot = 0
+    for (x0,y0), (x1,y1) in combinations(positions,2):
+        distance = abs(x0-x1) + abs(y0-y1)
+        tot += distance
+    return tot
+
+p1 = total_distance(expand_galaxy(data,1))
+p2 = total_distance(expand_galaxy(data,1000000-1))
+print(f"Answer part 1: {p1}")
+print(f"Answer part 1: {p2}")
