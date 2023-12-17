36fd55769da9ebff97c1e78aed72d3d368143d50
unknown <philipsi@axis.com>
2023-12-17 09:57:48 +0100

    day15 complete

--- 2023/day15/day15.py
+++ 2023/day15/day15.py
@@ -1,24 +1,14 @@
 from pathlib import Path
-import numpy as np
-import re
-
-def parse_line(line):
-    m = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
-    lo, hi, char, password = m.groups()
-    return int(lo), int(hi), char, password
+from collections import defaultdict
 
 def hashify(inp):
     val = 0
     for char in inp:
         val += ord(char)
         val *= 17
-        val = val % 256
-    # print(val)
+        val %= 256
     return val
-with open(Path("2023") / "day15" / "day15_input.txt") as f:
-# with open(Path("2023") / "day15" / "day15_test.txt") as f:
-    data = [line for line in f.read().split(',')]
-    # data = [parse_line(line) for line in f.read().split('\n')]
+
 assert hashify('HASH') == 52
 assert hashify('rn=1') == 30
 assert hashify('cm-') == 253
@@ -31,7 +21,45 @@ assert hashify('ab=5') == 197
 assert hashify('pc-') == 48
 assert hashify('pc=6') == 214
 assert hashify('ot=7') == 231
-def part1(data):
-    print(sum([hashify(x) for x in data]))
 
-part1(data)
+def calc_power(hashmap):
+    tot_power= 0
+    for box_nr, lenses in hashmap.items():
+        # print(box_nr, lenses)
+        for pos, (label, foc_len) in enumerate(lenses.items(),start=1):
+            val = (box_nr+1) * pos * foc_len
+            # print(label, val)
+            tot_power += val
+    return tot_power
+
+def part1(data):
+    return sum([hashify(x) for x in data])
+
+# Hurray for dicts being insert ordered
+def part2(data):
+    hashmap = defaultdict(dict)
+    for cmd in data:
+        if '-' in cmd:
+            label = cmd[:-1]
+            box_nr = hashify(label)
+            # print(label, hashify(label))
+            box = hashmap[box_nr]
+            if label in box:
+                del box[label]
+        elif '=' in cmd:
+            label, val = cmd.split('=')
+            box_nr = hashify(label)
+            box = hashmap[box_nr]
+            box[label] = int(val)
+        else:
+            raise RuntimeError('Uh oh')
+        # print('After ' + cmd)
+        # print(hashmap)
+    return calc_power(hashmap)
+
+with open(Path("2023") / "day15" / "day15_input.txt") as f:
+# with open(Path("2023") / "day15" / "day15_test.txt") as f:
+    data = [line for line in f.read().split(',')]
+
+print(f'Answer part1: {part1(data)}')
+print(f'Answer part2: {part2(data)}')
