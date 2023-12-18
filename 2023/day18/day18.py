8c49d85c00b2ccb5fe2b422f7f3872651b1ef65b
unknown <philipsi@axis.com>
2023-12-18 18:04:23 +0100

    day18 part1

--- 2023/day18/day18.py
+++ 2023/day18/day18.py
@@ -14,14 +14,42 @@ def parse_line(line):
     dir, val, hex,  = line.split(' ')
     return    dir, int(val), hex
 
-# with open(Path("2023") / "day18" / "day18_input.txt") as f:
-with open(Path("2023") / "day18" / "day18_test.txt") as f:
+with open(Path("2023") / "day18" / "day18_input.txt") as f:
+# with open(Path("2023") / "day18" / "day18_test.txt") as f:
     # data = [line for line in f.read().split('\n')]
     data = [parse_line(line) for line in f.read().split('\n')]
 def print_dig(dug):
     maxx = max([x for x,y in dug])
     maxy = max([y for x,y in dug])
-    digmap = np.zeros((maxy,maxx))
+    digmap = np.zeros((maxy+1,maxx+1),dtype=str)
+    digmap[:] = '.'
+    for x,y in dug:
+        digmap[y,x] = '#'
+    return digmap
+
+def fill(dug):
+    x,y = 1,1
+    while (x,y) in dug:
+        y +=1
+    print(x,y)
+    pos_to_fill = [(x,y)]
+    dug.add((x,y))
+    while pos_to_fill:
+        p = pos_to_fill.pop()
+        # d = print_dig(dug)
+        # d[p[1],p[0]] = 'O'
+        # print(d)
+        # print('\n')
+        for neighbor in neighbors(p):
+            if neighbor not in dug:
+                pos_to_fill.append(neighbor)
+                dug.add(neighbor)
+    return dug
+
+def neighbors(pos):
+    for i in [-1,1]:
+        yield pos[0] + i, pos[1]
+        yield pos[0] , pos[1]+i
 
 
 print(data)
@@ -29,7 +57,14 @@ pos = (0,0)
 dug = set()
 dug.add(pos)
 for dir, val, hex in data:
-    x,y  = dirmap[dir]
-    pos = (pos[0] + x, pos[1] + y)
-    dug.add(pos)
+    for i in range(val):
+        x,y  = dirmap[dir]
+        pos = (pos[0] + x, pos[1] + y)
+        dug.add(pos)
+        print(pos)
 print(dug)
+print_dig(dug)
+d=fill(dug)
+print_dig(d)
+print(list(neighbors((1,1))))
+print(len(d))
