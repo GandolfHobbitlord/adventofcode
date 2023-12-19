caa0952c3595dd64c3a95a16d9606e4db928245e
unknown <philipsi@axis.com>
2023-12-19 20:54:12 +0100

    day19 complete

--- 2023/day19/day19_2.py
+++ 2023/day19/day19_2.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import numpy as np
 import re
+from copy import deepcopy
 
 # This years dirty eval!
 def parse_part(part):
@@ -9,7 +10,6 @@ def parse_part(part):
     return eval(part)
 
 def parse_workflow(wf):
-
     key, rest = wf.split('{')
     rules = rest[:-1].split(',')
     out_rules = []
@@ -23,15 +23,35 @@ def parse_workflow(wf):
     out_rules.append(rules[-1])
     return key, out_rules
 
+# Dear diary, today i made spaghetti
+# Tactic: create a new range for each logical operator, continue parsing as if the statment was false
+#         and create a new range with destination for each that is true
+
 def get_next(part,rules):
+    possible_out = []
     for cat, operator, num, dest in rules[:-1]:
+        mini, maxi = part[cat]
         if operator =='<':
-            if part[cat] < num:
-                return dest
+            new_max = min(maxi, num-1)
+            if new_max > mini:
+                new_part = deepcopy(part)
+                new_part[cat] = (mini,new_max)
+                possible_out.append((dest,new_part))
+            mini = num
         elif operator == '>':
-            if part[cat] > num:
-                return dest
-    return rules[-1]
+            new_min = max(mini, num+1)
+            if new_min < maxi:
+                new_part = deepcopy(part)
+                new_part[cat] = (new_min,maxi)
+                possible_out.append((dest,new_part))
+            maxi = num
+        if mini < maxi:
+            part[cat] = (mini,maxi)
+        else:
+            return possible_out
+    if maxi > mini:
+        possible_out.append((rules[-1], deepcopy(part)))
+    return possible_out
 
 with open(Path("2023") / "day19" / "day19_input.txt") as f:
 # with open(Path("2023") / "day19" / "day19_test.txt") as f:
@@ -40,12 +60,33 @@ parts = [parse_part(part) for part in parts.splitlines()]
 wfs = dict([parse_workflow(wf) for wf in wfs.splitlines()])
 print(wfs)
 ans = 0
+part = {c: (1,4000) for c in 'xmas'}
+print(part)
+minimax = [('in' , part)]
+passed = []
+while minimax:
+    dest, part = minimax.pop()
+    new_possible = get_next(part,wfs[dest])
+    for key, val in new_possible:
+        if key == 'A':
+            passed.append(val)
+        elif key == 'R':
+            continue
+        else:
+            minimax.append((key,val))
+    print('hello')
 
-for part in parts:
-    loc = 'in'
-    while not (loc == 'A' or loc == 'R'):
-        loc = get_next(part, wfs[loc])
-    if loc == 'A':
-        ans += sum(part.values())
+print(passed)
+# vals = [minimax]
+p2 = 0
+for part in passed:
+    v = 1
+    for lo, hi in part.values():
+        v *= hi - lo + 1
+    p2 += v
+print(p2)
+# while vals:
+#     key, mm = vals.pop()
+#     get_next(mm,wfs[key])
 
-print(ans)
+# print(ans)
