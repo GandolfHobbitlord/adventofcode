8429fd7980a41357ca099e132f719f90b7b7e018
Philip Siederer <philipsi@axis.com>
2024-12-02 07:49:32 +0100

    cleanup
    Change-Id: Ib9f422693c6b149a52611502bcba2a66815f768c

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,15 +1,9 @@
 from pathlib import Path
 import numpy as np
 import re
-from collections import Counter
-from collections import defaultdict
 
 def parse_line(line):
-    # print(line)
     return [int(i) for i in re.findall(r'-?\d+', line)]
-    m = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
-    lo, hi, char, password = m.groups()
-    return int(lo), int(hi), char, password
 
 def is_safe(l):
     d = [x-y for x,y in zip(l,l[1:])]
@@ -33,7 +27,6 @@ def solve(data,part2=False):
     return ans
 with open(Path("2024") / "day2" / "day2_input.txt") as f:
 # with open(Path("2024") / "day2" / "day2_test.txt") as f:
-    # data = [line for line in f.read().split('\n')]
     data = [parse_line(line) for line in f.read().split('\n')]
     ans = 0
 
