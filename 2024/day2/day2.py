b73701e931b4af0aaec80f46dea71d4bf364b2fe
Philip Siederer <philipsi@axis.com>
2024-12-02 08:00:16 +0100

    cleanup
    Change-Id: Ieb1ed03a3328492e76427b68e141626630497ca5

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -10,7 +10,7 @@ def is_safe(l):
     # print(d)
     d = np.array(d)
     safe = np.all(d < 0) | np.all(d > 0)
-    safe = safe and np.all(np.abs(d) < 4) and np.all(d != 0)
+    safe = safe and np.all(np.abs(d) < 4)
     return safe
 
 def solve(data,part2=False):
@@ -25,8 +25,8 @@ def solve(data,part2=False):
                     ans += 1
                     break
     return ans
-# with open(Path("2024") / "day2" / "day2_input.txt") as f:
-with open(Path("2024") / "day2" / "day2_test.txt") as f:
+with open(Path("2024") / "day2" / "day2_input.txt") as f:
+# with open(Path("2024") / "day2" / "day2_test.txt") as f:
     data = [parse_line(line) for line in f.read().split('\n')]
     ans = 0
 
