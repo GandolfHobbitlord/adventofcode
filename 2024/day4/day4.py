05969bee872e22496e2643eb05b713eff8f8654d
unknown <philipsi@axis.com>
2024-12-04 06:55:41 +0100

    day4

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -1,14 +1,6 @@
 from pathlib import Path
 import numpy as np
-import re
 from collections import Counter
-from collections import defaultdict
-
-def parse_line(line):
-    # [int(i) for i in re.findall(r'-?\d+', line)]
-    m = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
-    lo, hi, char, password = m.groups()
-    return int(lo), int(hi), char, password
 
 def find_xmas(mat):
     ans = 0
