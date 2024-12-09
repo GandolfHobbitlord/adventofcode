2d1edf99912f99a39a264a7cbecdd33df4576b9b
unknown <philipsi@axis.com>
2024-12-09 20:08:21 +0100

    day5 complete

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,12 +1,11 @@
 from pathlib import Path
-import numpy as np
 import re
-from collections import Counter
 from collections import defaultdict
+from functools import cmp_to_key
 
-def parse_line(line):
+def parse_q(line):
     return [int(i) for i in re.findall(r'-?\d+', line)]
-    return int(lo), int(hi), char, password
+
 def parse_rules(rules):
     r = defaultdict(list)
     for rule in rules.splitlines():
@@ -14,26 +13,45 @@ def parse_rules(rules):
         r[a].append(b)
     return r
 
-# def parse_blob(blob):
-with open(Path("2024") / "day5" / "day5_input.txt") as f:
 
+with open(Path("2024") / "day5" / "day5_input.txt") as f:
 # with open(Path("2024") / "day5" / "day5_test.txt") as f:
     rules, blob = f.read().split('\n\n')
-    # data = [parse_line(line) for line in f.read().split('\n')]
-qs = [parse_line(l) for l in blob.splitlines()]
+qs = [parse_q(l) for l in blob.splitlines()]
 rules = parse_rules(rules)
-middles = []
-for q in qs:
-    valid = True
+
+def is_valid(q):
     for i in reversed(range(1,len(q))):
         a = set(rules[q[i]])
         b = set(q[:i])
         if a & b:
-            print(f'{q} is invalid')
-            valid = False
-            break
-    if valid:
-        middles.append(q[len(q)//2])
-print(qs)
-print(rules)
-print(sum(middles))
+            return False
+    return True
+
+
+def part1(qs):
+    middles = []
+    for q in qs:
+        if is_valid(q):
+            middles.append(q[len(q)//2])
+    return sum(middles)
+
+# Not sure if this covers all cases but it works...
+def comp_pages(a,b):
+    if b in rules[a]:
+        return -1
+    elif a in rules[b]:
+        return 1
+    else:
+        return 0
+
+def part2(qs):
+    middles = []
+    for q in qs:
+        if not is_valid(q):
+            s = sorted(q,key=cmp_to_key(comp_pages))
+            middles.append(s[len(s)//2])
+    return sum(middles)
+
+print(part1(qs))
+print(part2(qs))
