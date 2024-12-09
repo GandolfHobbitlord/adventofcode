c575650646a0d70488e49acb5827822445b006fd
Philip Siederer <philipsi@axis.com>
2024-12-09 19:37:36 +0100

    day5 part1

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -15,17 +15,25 @@ def parse_rules(rules):
     return r
 
 # def parse_blob(blob):
-# with open(Path("2024") / "day5" / "day5_input.txt") as f:
+with open(Path("2024") / "day5" / "day5_input.txt") as f:
 
-with open(Path("2024") / "day5" / "day5_test.txt") as f:
+# with open(Path("2024") / "day5" / "day5_test.txt") as f:
     rules, blob = f.read().split('\n\n')
     # data = [parse_line(line) for line in f.read().split('\n')]
 qs = [parse_line(l) for l in blob.splitlines()]
 rules = parse_rules(rules)
-
+middles = []
 for q in qs:
-    for i in range()
+    valid = True
+    for i in reversed(range(1,len(q))):
+        a = set(rules[q[i]])
+        b = set(q[:i])
+        if a & b:
+            print(f'{q} is invalid')
+            valid = False
+            break
+    if valid:
+        middles.append(q[len(q)//2])
 print(qs)
 print(rules)
-
-
+print(sum(middles))
