70f73f13904ffc2bb1aeb81e1294e2aae1396ec8
unknown <philipsi@axis.com>
2024-12-15 16:49:50 +0100

    day13 complete

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 import numpy as np
 import re
-from collections import Counter
-from collections import defaultdict
 
-def parse_board(board):
+def parse_board(board,shift):
     board = board.splitlines()
     top = [int(i) for i in re.findall(r'-?\d+', board[0])]
     bottom = [int(i) for i in re.findall(r'-?\d+', board[1])]
-    b = np.array([int(i) for i in re.findall(r'-?\d+', board[2])])
-
+    b = np.array([int(i) + shift for i in re.findall(r'-?\d+', board[2])])
     A = np.array((top,bottom)).T
     return A,b
 
@@ -17,11 +14,11 @@ with open(Path("2024") / "day13" / "day13_input.txt") as f:
 # with open(Path("2024") / "day13" / "day13_test.txt") as f:
     data = [board for board in f.read().split('\n\n')]
 
-def part1(data):
-    boards = [parse_board(b) for b in data]
+def solve(data,shift = 0):
+    boards = [parse_board(b,shift) for b in data]
     solves = [np.linalg.solve(A,b) for A, b in boards]
-    pruned = [solve.astype(np.int16) for solve in solves if np.all(np.isclose(solve,np.round(solve)))]
-    print(sum([np.dot((3,1),s) for s in pruned]))
+    pruned = [solve for solve in solves if np.all(np.isclose(solve,np.round(solve),atol=0.01,rtol=0))]
+    return round(sum([np.dot((3,1),s) for s in pruned]))
 
-# part1(data)
-part1(data)
+print(f'Answer part1: {solve(data)}')
+print(f'Answer part2: {solve(data,shift=10000000000000)}')
