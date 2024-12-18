3f5c0ceae9e82f29b98659348f52f390f529f0b3
unknown <philipsi@axis.com>
2024-12-18 20:00:40 +0100

    day17 complete

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -6,13 +6,13 @@ from collections import defaultdict
 
 
 
-def run(program,A,B,C):
+def run(program,A,B=0,C=0):
     idx = 0
     out = []
     val = [0,1,2,3,A,B,C]
     while idx < len(program):
         op = program[idx]
-        print(f'opcode {op}')
+        # print(f'opcode {op}')
         if op == 0:
             combo = program[idx+1]
             val[4] = val[4] >> val[combo]
@@ -57,7 +57,39 @@ def run_tests():
     assert res[5] == 44354
     o, res =run(program=[0,1,5,4,3,0],A=729,B=0,C=0)
     assert o == [4,6,3,5,6,3,5,2,1,0]
-run_tests()
+# run_tests()
+
+#Misread and thought B,C mattered so thought i had to do it in correct order. not sure why it isn't functioning though...
+# def recursive_solve(program,shifts=0,A=0):
+#     if shifts == len(program):
+#         print(program)
+#         return A
+#     for i in range(8):
+#         test_a = (i << (3*shifts)) | A
+#         out, _ = run(program,test_a)
+#         if program[:shifts+1] == out:
+#             res = recursive_solve(program,shifts+1,A=test_a)
+#             if res:
+#                 return res
+#     return None
+
+ans = []
+#B,C have no effect, just A
+def recursive_solve(program,shifts=0,A=0):
+    if shifts == len(program):
+        ans.append(A)
+        return A
+    for test in range(8):
+        test_a = (A<< 3) | test
+        out, _ = run(program, test_a)
+        if out == program[-1*(1+shifts):]:
+            recursive_solve(program,shifts+1,test_a)
+
+# o, res =run(program=[2,4,1,1,7,5,0,3,1,4,4,0,5,5,3,0],A=32916674,B=0,C=0)
+recursive_solve(A=0,program=[2,4,1,1,7,5,0,3,1,4,4,0,5,5,3,0])
+
+# print(o)
+#Part 1
 o, res =run(program=[2,4,1,1,7,5,0,3,1,4,4,0,5,5,3,0],A=32916674,B=0,C=0)
-print(o)
-print(",".join(str(x) for x in o))
+print( "part 1 "+",".join(str(x) for x in o))
+print(f"Part 2 {sorted(ans)[0]}")
