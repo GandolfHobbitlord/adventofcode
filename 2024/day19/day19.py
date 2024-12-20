c6306cac5dba8d02042a33556cc8bf2e96cee9b4
Philip Siederer <philipsi@axis.com>
2024-12-20 18:44:32 +0100

    day19 complete

--- 2024/day19/day19.py
+++ 2024/day19/day19.py
@@ -1,9 +1,7 @@
 from pathlib import Path
-import numpy as np
 import re
-from collections import Counter
-from collections import defaultdict
 import functools
+
 def parse_towles(line):
     return [i for i in re.findall(r'\w+', line)]
 
@@ -14,19 +12,20 @@ with open(Path("2024") / "day19" / "day19_input.txt") as f:
     towels = set(parse_towles(lines[0]))
     patterns = lines[2:]
 
+#Python felt like cheating today
 @functools.cache
-def parse(pattern):
+def parse2(pattern):
     if pattern == '':
-        return True
+        return 1
     possible =  [towl for towl in towels if pattern.startswith(towl)]
+    res = 0
     for p in possible:
         s = pattern[len(p):]
-        res = parse(s)
-        if res:
-            return True
-    return False
+        res += parse2(s)
+    return res
 
-for pattern in patterns:
-    # print(pattern)
-    print(parse(pattern),pattern)
-print(sum([parse(pattern) for pattern in patterns]))
+ans = [parse2(pattern) for pattern in patterns]
+ans1 =  sum([p != 0 for p in ans])
+ans2 =  sum(ans)
+print(f'anwers part 1: {ans1}')
+print(f'anwers part 2: {ans2}')
