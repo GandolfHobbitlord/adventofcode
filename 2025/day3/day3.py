e86733a2cd1c1ab64f1040b21e798a5d7b63ab41
unknown <philipsi@axis.com>
2025-12-03 06:33:13 +0100

    day3 cleanup

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 
 def parse_line(line):
     return [int(i) for i in line]
-    return int(lo), int(hi), char, password
 
 with open(Path("2025") / "day3" / "day3_input.txt") as f:
 # with open(Path("2025") / "day3" / "day3_test.txt") as f:
@@ -20,22 +19,17 @@ def get_largest(line, pos,tot):
     else:
         largest = sorted(line)[-1]
     i = line.index(largest)
-    return largest,line[i+1:]
+    return largest, line[i+1:]
 
-ans = []
-tot = 12
-for line in data:
-    s = ""
-    curr_ind = 0
-    l = line.copy()
-    for pos in range(tot):
-        largest,l = get_largest(l,pos,tot)
-        s+=str(largest)
-    ans.append(int(s))
-    # second = sorted(line[i+1:])[-1]
-    # res = int(str(largest) + str(second))
-    # print(res)
-    # ans += res
-print(ans)
-print(sum(ans))
-# print(ans)
+def get_joltage(data,num_batteries):
+    ans = []
+    for line in data:
+        s = ""
+        l = line.copy()
+        for pos in range(num_batteries):
+            largest,l = get_largest(l,pos,num_batteries)
+            s+=str(largest)
+        ans.append(int(s))
+    return sum(ans)
+print(f"Total joltage part 1: {get_joltage(data,2)}")
+print(f"Total joltage part 2: {get_joltage(data,12)}")
