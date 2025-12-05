4867f3f571fae0e560155e424b684742f64d2a9a
unknown <philipsi@axis.com>
2025-12-05 07:08:30 +0100

    day5 complete

--- 2025/day5/day5.py
+++ 2025/day5/day5.py
@@ -1,8 +1,5 @@
 from pathlib import Path
-import numpy as np
 import re
-from collections import Counter
-from collections import defaultdict
 
 def parse_line(line):
     return [int(i) for i in re.findall(r'\d+', line)]
@@ -14,17 +11,31 @@ with open(Path("2025") / "day5" / "day5_input.txt") as f:
     fruit = [int(f) for f in fruit.split('\n')]
 
 
-def is_fresh(fruit,ranges):
+def is_fresh(fruit, ranges):
     for lo, hi in ranges:
         if lo <= fruit and hi >= fruit:
             return True
     return False
-ans = 0
-print(ranges)
-print(fruit)
-for f in fruit:
-    if is_fresh(f,ranges):
-        # print("fresh", f)
-        ans += 1
 
-print(ans)
+def part1(ranges, fruit):
+    ans = 0
+    for f in fruit:
+        if is_fresh(f,ranges):
+            ans += 1
+    return ans
+
+def part2(ranges):
+    orig_ranges = ranges.copy()
+    orig_ranges.sort(reverse=True)
+    merged = [orig_ranges.pop()]
+    while orig_ranges:
+        r_lo,r_hi = orig_ranges.pop()
+        if r_lo <= merged[-1][1]:
+            merged[-1][1] = max(r_hi,merged[-1][1])
+        else:
+            merged.append([r_lo,r_hi])
+    return sum([hi - lo + 1 for lo, hi in merged])
+
+print(f"Answer part1: {part1(ranges,fruit)}")
+print(f"Answer part2: {part2(ranges)}")
+
